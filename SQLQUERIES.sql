079d70a8a23c95f699cd2907a1e261a6aa53e8bf
slamrow@gmail.com <#1WildCat>
2018-09-28 22:48:37 -0600

    Queries for assignment one

--- SQLQUERIES.sql
+++ SQLQUERIES.sql
@@ -1,15 +1,67 @@
 /* This file is for the sql queries */
+--1
 SELECT
-	*
+	(ConsoleBrand + ConsoleModel) AS [Consoles]
 FROM
-	dbo.games;
+	dbo.Console_Ref
+;
 
+--2
 SELECT
-	*
+	GameTitle
 FROM
-	dbo.Consoles;
+	dbo.Games_Ref
+;
 
+--3
 SELECT
-	*
+	GR.GameTitle,
+	CR.ConsoleModel
+
 FROM
-	dbo.Purchases;
+	dbo.Games G
+	LEFT JOIN dbo.Games_Ref GR ON G.GameRefID = GR.GameRefID
+	LEFT JOIN dbo.Console_Ref CR ON CR.ConsoleRefID = G.ConsoleRefID
+
+WHERE
+	CR.ConsoleModel = 'Xbox One'
+;
+
+--4
+SELECT
+	GT.GameTitle,
+	(CR.ConsoleBrand + ' ' + CR.ConsoleModel) As [Console]
+		
+FROM
+	(SELECT 
+		GR.GameTitle
+	
+	
+		FROM
+			dbo.Games G
+			LEFT JOIN dbo.Games_Ref GR ON G.GameRefID = GR.GameRefID
+			LEFT JOIN dbo.Console_Ref CR ON CR.ConsoleRefID = G.ConsoleRefID
+
+		GROUP BY
+			GR.GameTitle
+
+		HAVING
+			count(GR.GameTitle) > 1) GT
+	LEFT JOIN dbo.Games_Ref GR ON GT.GameTitle = GR.GameTitle
+	LEFT JOIN dbo.Games G ON G.GameRefID = GR.GameRefID
+	LEFT JOIN dbo.Console_Ref CR ON CR.ConsoleRefID = G.ConsoleRefID
+;
+
+--5
+SELECT 
+	GR.GameTitle,
+	(CR.ConsoleBrand + ' ' + CR.ConsoleModel) As [Console]
+		
+FROM
+	dbo.Games_Ref GR 
+	LEFT JOIN dbo.Games G ON G.GameRefID = GR.GameRefID
+	LEFT JOIN dbo.Console_Ref CR ON CR.ConsoleRefID = G.ConsoleRefID
+
+WHERE
+	GR.Genre like'%Role-Playing%'
+;
